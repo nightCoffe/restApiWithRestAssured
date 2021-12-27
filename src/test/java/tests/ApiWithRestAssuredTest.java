ab3cc4e01d58dee2d55feeb0122a33482116b3bb
Your Name <youremail@domain.com>
2021-12-27 02:34:51 -0800

    added test on List resource

--- src/test/java/tests/ApiWithRestAssuredTest.java
+++ src/test/java/tests/ApiWithRestAssuredTest.java
@@ -21,13 +21,13 @@ public class ApiWithRestAssuredTest {
     @Test
     @DisplayName("List users")
     void listUser() {
-        String responseData = "{\"page\":2,\"per_page\":6,\"total\":12,\"total_pages\":2,\"data\":[{\"id\":7,\"email\":\"michael.lawson@reqres.in\",\"first_name\":\"Michael\",\"last_name\":\"Lawson\",\"avatar\":\"https://reqres.in/img/faces/7-image.jpg\"},{\"id\":8,\"email\":\"lindsay.ferguson@reqres.in\",\"first_name\":\"Lindsay\",\"last_name\":\"Ferguson\",\"avatar\":\"https://reqres.in/img/faces/8-image.jpg\"},{\"id\":9,\"email\":\"tobias.funke@reqres.in\",\"first_name\":\"Tobias\",\"last_name\":\"Funke\",\"avatar\":\"https://reqres.in/img/faces/9-image.jpg\"},{\"id\":10,\"email\":\"byron.fields@reqres.in\",\"first_name\":\"Byron\",\"last_name\":\"Fields\",\"avatar\":\"https://reqres.in/img/faces/10-image.jpg\"},{\"id\":11,\"email\":\"george.edwards@reqres.in\",\"first_name\":\"George\",\"last_name\":\"Edwards\",\"avatar\":\"https://reqres.in/img/faces/11-image.jpg\"},{\"id\":12,\"email\":\"rachel.howell@reqres.in\",\"first_name\":\"Rachel\",\"last_name\":\"Howell\",\"avatar\":\"https://reqres.in/img/faces/12-image.jpg\"}],\"support\":{\"url\":\"https://reqres.in/#support-heading\",\"text\":\"To keep ReqRes free, contributions towards server costs are appreciated!\"}}";
+        String responseDataListUser = "{\"page\":2,\"per_page\":6,\"total\":12,\"total_pages\":2,\"data\":[{\"id\":7,\"email\":\"michael.lawson@reqres.in\",\"first_name\":\"Michael\",\"last_name\":\"Lawson\",\"avatar\":\"https://reqres.in/img/faces/7-image.jpg\"},{\"id\":8,\"email\":\"lindsay.ferguson@reqres.in\",\"first_name\":\"Lindsay\",\"last_name\":\"Ferguson\",\"avatar\":\"https://reqres.in/img/faces/8-image.jpg\"},{\"id\":9,\"email\":\"tobias.funke@reqres.in\",\"first_name\":\"Tobias\",\"last_name\":\"Funke\",\"avatar\":\"https://reqres.in/img/faces/9-image.jpg\"},{\"id\":10,\"email\":\"byron.fields@reqres.in\",\"first_name\":\"Byron\",\"last_name\":\"Fields\",\"avatar\":\"https://reqres.in/img/faces/10-image.jpg\"},{\"id\":11,\"email\":\"george.edwards@reqres.in\",\"first_name\":\"George\",\"last_name\":\"Edwards\",\"avatar\":\"https://reqres.in/img/faces/11-image.jpg\"},{\"id\":12,\"email\":\"rachel.howell@reqres.in\",\"first_name\":\"Rachel\",\"last_name\":\"Howell\",\"avatar\":\"https://reqres.in/img/faces/12-image.jpg\"}],\"support\":{\"url\":\"https://reqres.in/#support-heading\",\"text\":\"To keep ReqRes free, contributions towards server costs are appreciated!\"}}";
         String response =
                 get("/api/users?page=2")
                         .then()
                         .statusCode(200)
                         .extract().response().asString();
-        assertThat(response).isEqualTo(responseData);
+        assertThat(response).isEqualTo(responseDataListUser);
     }
 
     @Test
@@ -80,6 +80,17 @@ public class ApiWithRestAssuredTest {
                 .post("/api/register")
                 .then()
                 .statusCode(200)
-                .body(  "id", is(4),"token", is("QpwL5tke4Pnpja7X4") );
+                .body("id", is(4), "token", is("QpwL5tke4Pnpja7X4"));
+    }
+
+    @Test
+    @DisplayName("List <resource>")
+    public void listResource() {
+        given()
+                .when()
+                .get("/api/unknown")
+                .then()
+                .statusCode(200)
+                .body("page", is(1), "per_page", is(6), "total", is(12), "total_pages", is(2));
     }
 }
